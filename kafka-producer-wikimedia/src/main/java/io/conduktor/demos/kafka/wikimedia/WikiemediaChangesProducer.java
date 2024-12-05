09f357fab87143c59836427675b7fa37c9327bce
mkhmmd17 <maxmelhi95@gmail.com>
2024-12-04 20:13:14 -0500

    Created Stream + Different Filter Topics

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikiemediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikiemediaChangesProducer.java
@@ -12,30 +12,34 @@ import java.util.concurrent.TimeUnit;
 
 public class WikiemediaChangesProducer {
     public static void main(String[] args) throws InterruptedException {
-        String bootstrapServers = "pkc-619z3.us-east1.gcp.confluent.cloud:9092";
-
+//        String bootstrapServers = "pkc-619z3.us-east1.gcp.confluent.cloud:9092";
         //create Producer properties
         Properties properties = new Properties();
 
         // Kafka cluster connection settings
-        properties.setProperty("bootstrap.servers", bootstrapServers);
-        properties.setProperty("security.protocol", "SASL_SSL"); // Correct protocol
-        properties.setProperty("sasl.mechanism", "PLAIN");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // Inline JAAS configuration
-        properties.setProperty("sasl.jaas.config",
-                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
-                        "username=\"LWFF2XF2MS4SXMOP\" " +
-                        "password=\"UM4XUAujhKjU7NKwtgNln5nUDdzUbs6oV9SFnkFAqBWZU2umnyU7IMoP3LIfrRmR\";");
-
-        // Serializer settings
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        //CONFLUENT CLUSTER
+//        properties.setProperty("bootstrap.servers", bootstrapServers);
+//        properties.setProperty("security.protocol", "SASL_SSL"); // Correct protocol
+//        properties.setProperty("sasl.mechanism", "PLAIN");
+//
+//        // Inline JAAS configuration
+//        properties.setProperty("sasl.jaas.config",
+//                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+//                        "username=\"LWFF2XF2MS4SXMOP\" " +
+//                        "password=\"UM4XUAujhKjU7NKwtgNln5nUDdzUbs6oV9SFnkFAqBWZU2umnyU7IMoP3LIfrRmR\";");
+//
+//        // Serializer settings
+//        properties.setProperty("key.serializer", StringSerializer.class.getName());
+//        properties.setProperty("value.serializer", StringSerializer.class.getName());
 
         // set high throughput producer configs
-        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
-        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
-        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+//        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+//        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+//        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
